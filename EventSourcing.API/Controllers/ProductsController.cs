85f520eaa5dab80e6b06784cc2755525002069c6
Faruk Çökmez <hfcokmez@gmail.com>
2022-04-12 22:47:47 +0300

    Swagger implementation and ProductController are written.

---
+++ EventSourcing.API/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventSourcing.API.Commands;
+using EventSourcing.API.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+ 
+namespace EventSourcing.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class ProductsController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductDTO createProductDTO)
+        {
+            await _mediator.Send(new CreateProductCommand() { CreateProductDto = createProductDTO });
+            return NoContent();
+        }
+        [HttpPut]
+        public async Task<IActionResult> ChangeName(ChangeProductNameDTO changeProductNameDTO)
+        {
+            await _mediator.Send(new ChangeProductNameCommand() { ChangeProductNameDTO = changeProductNameDTO });
+            return NoContent();
+        } 
+        [HttpPut]
+        public async Task<IActionResult> ChangePrice(ChangeProductPriceDTO changeProductPriceDTO)
+        {
+            await _mediator.Send(new ChangeProductPriceCommand() { ChangeProductPriceDTO = changeProductPriceDTO });
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _mediator.Send(new DeleteProductCommand() { Id = id });
+            return NoContent();
+        }
+
+    }
+}
--- EventSourcing.API/Startup.cs
+++ EventSourcing.API/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using EventSourcing.API.EventStores;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace EventSourcing.API
 {
@@ -29,6 +32,40 @@ namespace EventSourcing.API
             services.AddControllers();
             services.AddEventStore(Configuration);
             services.AddSingleton<ProductStream>();
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Event Sourcing API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,8 +73,16 @@ namespace EventSourcing.API
         {
             if (env.IsDevelopment())
             {
+                app.UseSwagger();
+                app.UseSwaggerUI();
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
